061dcfd8323d786870ff6885c2159222c01df0e4
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-06-19 21:15:55 -0700

    Update nav

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,12 +1,12 @@
 var React = require('react')
 
-var PageOne = React.createClass({
+var Timer = React.createClass({
 
   render: function () {
     return (
-      <h1>Page One</h1>
+      <h1>Timer.jsx</h1>
     );
   }
 });
 
-module.exports = PageOne;
+module.exports = Timer;
